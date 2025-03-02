a6c0e3cb42b6ccf0111eb0988b904868c4069bd3
Joseph_11S <seguradiaz07@gmail.com>
2025-03-02 10:49:58 -0500

    Create pedir_datos.java
    esta es la forma de pedir datos de números de flotantes, doubles,string
    y Scanner es la forma de pedirle datos

---
+++ pedir_datos.java
@@ -0,0 +1,47 @@
+package trabajos_clase;
+
+//import java.util.Scanner;
+
+/*public class Entrada_salida_datos {
+    public static void main(String[] args) {
+       Scanner entrada = new Scanner(System.in); 
+        int numero;
+        System.out.print("Digite un numero:");
+        numero = entrada.nextInt();
+        System.out.println("El numero que digito es: "+numero);
+    }
+}*/
+
+//Se tiene que digitar con una cooma en vez de un punto
+/*public class Entrada_salida_datos {
+    public static void main(String[] args) {
+        
+        Scanner entrada=new Scanner(System.in);
+        float numero;
+        System.out.print("Digite un numero: ");
+        numero=entrada.nextFloat();
+        System.out.println("El numero que digito es: "+numero);
+
+    }
+}*/
+/*asi se  hace para pedirle un float al usuario el usurio digita un numero con coma y el programa lo toma como un float
+    public class Entrada_salida_datos {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double numero;
+        System.out.print("Digite un numero: ");
+        numero = entrada.nextDouble();
+        System.out.println("El numero que digito es: "+numero);
+      
+    }
+}*/
+/*aso se hace para pedirle un double al usuario el usuario digita un numero con coma y el programa lo toma como un double
+public class Entrada_salida_datos {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        String nombre;
+        System.out.print("Digite su nombre: ");
+        nombre = entrada.nextLine();
+        System.out.println("Su nombre es: "+nombre);
+    }
+}*/
